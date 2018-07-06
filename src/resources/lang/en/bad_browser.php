cc55d4dc261bc3a4f60983937db6421c00ac4f5e
Andrey Helldar <helldar@ai-rus.com>
2018-07-06 13:57:22 +0000

    Apply fixes from StyleCI

--- src/resources/lang/en/bad_browser.php
+++ src/resources/lang/en/bad_browser.php
@@ -1,23 +1,23 @@
 <?php
 
 return [
-    'use_bad' => 'You are using an outdated browser.',
-    'need_cookie' => 'To work with the site you need JavaScript, Cookies and a modern browser.',
-    'mistake' => 'If you think that we have incorrectly determined the version of your browser, then let us know by clicking on the "report us" button.',
-    'determined' => 'We defined your browser as <strong>:browser</strong> version <strong>:version</strong>.<br>To work on the site you need a browser version no lower than <strong>:need</strong>.',
+    'use_bad'         => 'You are using an outdated browser.',
+    'need_cookie'     => 'To work with the site you need JavaScript, Cookies and a modern browser.',
+    'mistake'         => 'If you think that we have incorrectly determined the version of your browser, then let us know by clicking on the "report us" button.',
+    'determined'      => 'We defined your browser as <strong>:browser</strong> version <strong>:version</strong>.<br>To work on the site you need a browser version no lower than <strong>:need</strong>.',
     'manual_redirect' => 'Did you personally forward this link, or did the site redirect you? If not, let us know by clicking on the button "Report us" at the bottom of the form, otherwise click the "To main" button.',
-    'sended' => 'The notification about your problem has been successfully sent to the administrator.',
-    'disabling' => 'If you still think this message is a mistake, click on the button to turn off its display on your computer.',
+    'sended'          => 'The notification about your problem has been successfully sent to the administrator.',
+    'disabling'       => 'If you still think this message is a mistake, click on the button to turn off its display on your computer.',
     'just_go_to_main' => 'Or just go to the main page.',
-    'install_that' => 'To use all the features of the site, download and install one of these browsers:',
-    'chrome' => 'Chrome',
-    'opera' => 'Opera',
-    'firefox' => 'Firefox',
-    'safari' => 'Safari',
+    'install_that'    => 'To use all the features of the site, download and install one of these browsers:',
+    'chrome'          => 'Chrome',
+    'opera'           => 'Opera',
+    'firefox'         => 'Firefox',
+    'safari'          => 'Safari',
 
     'buttons' => [
-        'main' => 'To main',
-        'report_us' => 'Report Us',
+        'main'           => 'To main',
+        'report_us'      => 'Report Us',
         'disable_notify' => 'Disable Notify',
     ],
 ];
